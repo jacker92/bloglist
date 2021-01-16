d923c3f6b01f7e090af85810be0c2334d6807b8e
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-16 22:05:53 +0200

    Added plugin info

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
         'node': true,
         'jest': true
     },
-    'extends': 'eslint:recommended',
+    'extends': 
+        ['eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:cypress/recommended',
+        "plugin:jest/recommended"],
     'parserOptions': {
         'ecmaVersion': 12
     },
