2a1012fed54de922af47ca6b6092bbf4a5d03b84
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-17 12:19:17 +0200

    Added default port for backend server

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
+if(!config.PORT) {
+    config.PORT = 3001
+}
+
 server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })
