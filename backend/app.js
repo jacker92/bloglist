d67c392535745ff69539b5ea8e43e8a0d26ce7c1
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-17 14:09:14 +0200

    Added build directory

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const config = require('./utils/config')
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
---
+++ backend/build/precache-manifest.f33ae4025f6565e76524cb6a72360f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1d0514e8c709e3722ac452e63be8f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9e0af030aa62de7ec6d",
+    "url": "/static/css/main.4eb0d69c.chunk.css"
+  },
+  {
+    "revision": "ca53e70ffac48efd4f2b",
+    "url": "/static/js/2.254376fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.254376fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e0af030aa62de7ec6d",
+    "url": "/static/js/main.ecfc69cd.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
