dc86af2b427930af7cf6c1780d211fcef6dd8f7e
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-17 13:28:31 +0000

    uibuild

--- backend/build/service-worker.js
+++ backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f33ae4025f6565e76524cb6a72360f6f.js"
+  "/precache-manifest.abc4cf58d5bbefcd90b53155c044ce15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
